58a246c4e985c5634338fd72b08b8d7a0c456088
MJ <mdjenkins655@gmail.com>
2022-01-16 04:10:11 -0500

    added footer icons/links

--- project-app/src/components/Footer.js
+++ project-app/src/components/Footer.js
@@ -1,11 +1,17 @@
 import React from "react";
+import {FaGithub} from "react-icons/fa"
+import {FaLinkedin} from "react-icons/fa"
+import {FaInstagram} from "react-icons/fa"
 
 export default function FooterMain() {
 
     return(
         <div class="container fixed-bottom footerbackground ">
         <footer class="text-center">
-            TESTING
+           <a href="https://github.com/JenkinsMD" target="_blank"> <FaGithub size="2em" color="black"/></a> 
+           <a href="https://www.linkedin.com/" target="_blank"> <FaLinkedin size="2em" color="black"/></a> 
+            <a href="https://www.instagram.com/beardedmcnugget/" target="_blank"> <FaInstagram size="2em" color="black"/></a> 
+
         </footer>
 
     </div>
